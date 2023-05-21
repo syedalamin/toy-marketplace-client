d91596a98b9e0def7285987fd6eae9b8c50bdc48
syedalamin <syedalamin2580@gmail.com>
2023-05-21 23:48:33 +0600

    login and register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,36 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h3>This is login</h3>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <form onSubmit={handleLogin}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className="my-4 text-center">New to Action Figure <Link to='/register' className="font-bold text-yellow-600">Register</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,46 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>register</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h1 className="text-5xl font-bold">Register </h1>
+                    <form onSubmit={handleRegister}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Register" />
+                        </div>
+                    </form>
+                    <p className="my-4 text-center">Already have an Account <Link to='/login' className="font-bold text-yellow-600">Login</Link></p>
+                </div>
+            </div>
         </div>
     );
 };

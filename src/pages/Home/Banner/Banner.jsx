9a6ea53a0a6e48e78903420b31b3170862fbf006
syedalamin <syedalamin2580@gmail.com>
2023-05-23 02:22:17 +0600

    banner done

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import bg from '../../../assets/image/bg.png';
+const Banner = () => {
+    return (
+        <div className='w-10/12 mx-auto my-10'>
+            <div className='grid sm:grid-cols-2'>
+                <div className='flex items-center justify-center'>
+                    <div>
+                        <p className='text-2xl pb-4 font-semibold'>Give The Gift Of <br /> Your Children Everyday</p>
+                        <h2 className='text-5xl font-bold'>Best <span className='text-teal-600'>Kids Toys</span> Store & <br /> Online Shop BD </h2>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center '>
+                    <div>
+                        <img className='' src={bg} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h3 className="text-5xl text-teal-500">this is home</h3>
+            <Banner></Banner>
         </div>
     );
 };
